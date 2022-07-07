78671ff9f97242a265714275d1c2ab8ebe69eaf3
hatskier <suvorovalex1997@gmail.com>
2022-07-07 18:02:18 +0200

    chore: disabled metrics saving in cloudwatch without removing metrics endpoint

--- helpers/cloudwatch.ts
+++ helpers/cloudwatch.ts
@@ -1,22 +1,26 @@
-import AWS from "aws-sdk";
+// import AWS from "aws-sdk";
 import { logger } from "../helpers/logger";
-import { isProduction } from "../config";
+// import { isProduction } from "../config";
 
 export const saveMetric = async ({ label, value }) => {
-  if (isProduction) {
-    const cloudwatch = new AWS.CloudWatch({apiVersion: "2010-08-01"});
+  // We disabled saving metrics in cloudwatch because of
+  // rapidly growing costs. We've switched to our self-hosted
+  // metrics storage in oracle-monitoring instances
+  logger.info("Metric saving in AWS cloudwatch skipped");
+  // if (isProduction) {
+  //   const cloudwatch = new AWS.CloudWatch({apiVersion: "2010-08-01"});
 
-    const metricParams = {
-      MetricData: [{
-        MetricName: label,
-        Value: value,
-      }],
-      Namespace: "CustomPerformanceMetrics",
-    };
+  //   const metricParams = {
+  //     MetricData: [{
+  //       MetricName: label,
+  //       Value: value,
+  //     }],
+  //     Namespace: "CustomPerformanceMetrics",
+  //   };
 
-    await cloudwatch.putMetricData(metricParams).promise();
-  } else {
-    logger.info(
-      "Metric saving in AWS cloudwatch skipped in non-prod environment");
-  }
+  //   await cloudwatch.putMetricData(metricParams).promise();
+  // } else {
+  //   logger.info(
+  //     "Metric saving in AWS cloudwatch skipped in non-prod environment");
+  // }
 };
